8fa686d4472c717d747d4b8bb7595236ad8e2f8e
LASCAUX Romain <lascauxr@hostname.iut2.upmf-grenoble.fr>
2018-03-19 15:59:15 +0100

    message'

---
+++ include/FrameOutil.h
@@ -0,0 +1,16 @@
+#ifndef FRAMEOUTIL_H
+#define FRAMEOUTIL_H
+
+
+class FrameOutil
+{
+    public:
+        FrameOutil();
+        virtual ~FrameOutil();
+
+    protected:
+
+    private:
+};
+
+#endif // FRAMEOUTIL_H
